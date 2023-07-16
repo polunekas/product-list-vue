89c57fe024d8b1efc9c123e3d8ad046283d3ee0d
polina <polune.kas@gmail.com>
2023-07-16 17:12:36 +0300

    feat: add methods and sorting by activity

--- app.js
+++ app.js
@@ -6,9 +6,30 @@ Vue.createApp({
 		}
 	},
 	computed: {
-		
+		sortedProducts() {
+		return this.products.sort((a,b) => {
+			if(!a.active && b.active) return -1;
+			if(a.active && b.active) return 0;
+			if(a.active && !b.active) return 1;
+		});
+		}
 	},
 	methods: {
+		saveProduct() {
+			if(this.productName === '') return
+			this.products.unshift({
+				name:this.productName,
+				active:false
+			});
+			this.productName = ''
+		},
+		toggleActive(product) {
+			product.active = !product.active
+		},
+		removeProduct(product) {
+            this.products.splice(this.products.indexOf(product), 1)
+        }
+
 	}
 
 }).mount('#app')
